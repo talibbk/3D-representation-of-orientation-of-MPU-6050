802f8c92e24132e3cb7fce7bd236ef667b30b35b
Talib Khurshid <b6278@oic.jp>
2019-07-17 11:25:57 +0900

    Store Data to CSV

--- boxctrl_6d0f_imu.py
+++ boxctrl_6d0f_imu.py
@@ -1,248 +1,216 @@
-// MPU-6050 Accelerometer + Gyro
+#!/usr/bin/env python
 
-// Bluetooth module connected to digital pins 2,3
-// I2C bus on A4, A5
-// Servo on pin 0
-//Servo is used here to check the authenticity of gyro data
-//Since we are not using servo, i have commented out codes related to servo
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import pygame
+from pygame.locals import *
+import serial
+import time
+import csv
 
-#include <Wire.h>
-//#include <SoftwareSerial.h>
-#include <math.h>
-//#include <Servo.h>
+#ser = serial.Serial('/dev/tty.usbserial', 38400, timeout=1)
+ser = serial.Serial('/dev/cu.usbmodem14101', 38400, timeout=1)
 
+ax = ay = az = 0.0
+yaw_mode = False
 
-#define MPU6050_I2C_ADDRESS 0x68 //as metntioned in datasheet section 4.32 and 9.2, if the pin(AD0) is LOW I2C adderess will be 0x68 and if the pin is HIGH address will be 0x69
+def resize(width, height):
+    if height==0:
+        height=1
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, 1.0*width/height, 0.1, 100.0)     #(field of view, aspect ratio, zNear, ZFar)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
-#define FREQ  30.0 // sample freq in Hz
+def init():
+    glShadeModel(GL_SMOOTH)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
-// Bluetooth transmitter, used optionally
-// SoftwareSerial BTSerial(2, 3); // RX | TX
+def drawText(position, textString):     
+    font = pygame.font.SysFont ("Courier", 18, True)
+    textSurface = font.render(textString, True, (255,255,255,255), (0,0,0,255))     
+    textData = pygame.image.tostring(textSurface, "RGBA", True)     
+    glRasterPos3d(*position)     
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
-//Servo roll_servo;
+def draw():
+    global rquad
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+    
+    glLoadIdentity()
+    glTranslatef(0,0.0,-7.0)
 
-// global angle, gyro derived
-double gSensitivity = 65.5; // for 500 deg/s, check data sheet
-double gx = 0, gy = 0, gz = 0;
-double gyrX = 0, gyrY = 0, gyrZ = 0;
-int16_t accX = 0, accY = 0, accZ = 0;
+    osd_text = "pitch: " + str("{0:.2f}".format(ay)) + ", roll: " + str("{0:.2f}".format(ax)) #displaying upto 2 decimal place
 
-double gyrXoffs = -281.00, gyrYoffs = 18.00, gyrZoffs = -83.00;
+    if yaw_mode:
+        osd_line = osd_text + ", yaw: " + str("{0:.2f}".format(az))
+    else:
+        osd_line = osd_text
 
-void setup()
-{      
-  int error;
-  uint8_t c;
-  uint8_t sample_div;
+    drawText((-2,-2, 2), osd_line) #position of text
 
-  //BTSerial.begin(38400);
-  Serial.begin(38400);
+    # the way I'm holding the IMU board, X and Y axis are switched 
+    # with respect to the OpenGL coordinate system
+    if yaw_mode:                             # experimental
+        glRotatef(az, 0.0, 1.0, 0.0)  # Yaw,   rotate around y-axis
+    else:
+        glRotatef(0.0, 0.0, 1.0, 0.0)
+    glRotatef(ay ,1.0,0.0,0.0)        # Pitch, rotate around x-axis
+    glRotatef(-1*ax ,0.0,0.0,1.0)     # Roll,  rotate around z-axis
+    
+    #Header for CSV
+    with open('test1.csv', 'a') as logfile:
+        fieldnames = ['Pitch','Roll','Yaw']
+        writer = csv.DictWriter(logfile, fieldnames = fieldnames)
 
-  // debug led
-  pinMode(13, OUTPUT);  //we are not using led
+    #Write header only if its not present  
+        if logfile.tell() == 0:
+            writer.writeheader()
 
-  // servo 
-  //roll_servo.attach(9, 550, 2550);
+    #Write data to CSV
+        writer.writerow({'Pitch':ay, 'Roll':ax, 'Yaw':az})
+      
+    print(osd_line)
 
-  // Initialize the 'Wire' class for the I2C-bus.
-  Wire.begin();
+    glBegin(GL_QUADS)	
+    #defining  faces of rectangle
+    glColor3f(0.0,1.0,0.0)
+    glVertex3f( 1.0, 0.2,-1.0)
+    glVertex3f(-1.0, 0.2,-1.0)		
+    glVertex3f(-1.0, 0.2, 1.0)		
+    glVertex3f( 1.0, 0.2, 1.0)		
 
-  // PWR_MGMT_1:
-  // wake up 
-  i2c_write_reg (MPU6050_I2C_ADDRESS, 0x6b, 0x00); //(accessing Power Management to wakeup the device : section 4.28)
+    glColor3f(1.0,0.5,0.0)	
+    glVertex3f( 1.0,-0.2, 1.0)
+    glVertex3f(-1.0,-0.2, 1.0)		
+    glVertex3f(-1.0,-0.2,-1.0)		
+    glVertex3f( 1.0,-0.2,-1.0)		
 
-  // CONFIG:
-  // Low pass filter samples, 1khz sample rate
-  i2c_write_reg (MPU6050_I2C_ADDRESS, 0x1a, 0x01);
+    glColor3f(1.0,0.0,0.0)		
+    glVertex3f( 1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2, 1.0)		
+    glVertex3f(-1.0,-0.2, 1.0)		
+    glVertex3f( 1.0,-0.2, 1.0)		
 
-  // GYRO_CONFIG:
-  // 500 deg/s, FS_SEL=1
-  // This means sensitivity 65.5 LSBs/deg/s
-  i2c_write_reg(MPU6050_I2C_ADDRESS, 0x1b, 0x08);
+    glColor3f(1.0,1.0,0.0)	
+    glVertex3f( 1.0,-0.2,-1.0)
+    glVertex3f(-1.0,-0.2,-1.0)
+    glVertex3f(-1.0, 0.2,-1.0)		
+    glVertex3f( 1.0, 0.2,-1.0)		
 
-  // CONFIG:
-  // set sample rate
-  // sample rate FREQ = Gyro sample rate / (sample_div + 1)
-  // 1kHz / (div + 1) = FREQ  
-  // reg_value = 1khz/FREQ - 1
-  sample_div = 1000 / FREQ - 1;
-  i2c_write_reg (MPU6050_I2C_ADDRESS, 0x19, sample_div);
+    glColor3f(0.0,0.0,1.0)	
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2,-1.0)		
+    glVertex3f(-1.0,-0.2,-1.0)		
+    glVertex3f(-1.0,-0.2, 1.0)		
 
+    glColor3f(1.0,0.0,1.0)	
+    glVertex3f( 1.0, 0.2,-1.0)
+    glVertex3f( 1.0, 0.2, 1.0)
+    glVertex3f( 1.0,-0.2, 1.0)		
+    glVertex3f( 1.0,-0.2,-1.0)		
+    glEnd()	
 
-//  Serial.write("Calibrating...");
-  digitalWrite(13, HIGH);
-  calibrate();
-  digitalWrite(13, LOW);
-//  Serial.write("done.");
-}
+def axes():
+    #Draw coordinate axes.
+    #Red = Positive X direction
+    #White = Positive Y direction
+    #Blue = Positive Z direction
 
-void loop()
-{
-  int error;
-  double dT;
-  double ax, ay, az;
-  unsigned long start_time, end_time;
+    glLineWidth(3.0)
+    glBegin(GL_LINES)
+    # X
+    glColor3f(1.0, 0.0, 0.0) # Red
 
-  start_time = millis();
+    glVertex3f(0.0, 0.0, 0.0)
+    glVertex3f(2.0, 0.0, 0.0)
 
-  read_sensor_data();
+    # arrow
+    glVertex3f(2.0, 0.0, 0.0)
+    glVertex3f(1.5, 0.5, 0.0)
 
-  // angles based on accelerometer
-  ay = atan2(accX, sqrt( pow(accY, 2) + pow(accZ, 2))) * 180 / M_PI;
-  ax = atan2(accY, sqrt( pow(accX, 2) + pow(accZ, 2))) * 180 / M_PI;
+    glVertex3f(2.0, 0.0, 0.0)
+    glVertex3f(1.5, -0.5, 0.0)
 
-  // angles based on gyro (deg/s)
-  gx = gx + gyrX / FREQ;
-  gy = gy - gyrY / FREQ;
-  gz = gz + gyrZ / FREQ;
+    # Y
+    glColor3f(1.0, 1.0, 1.0) # White
 
-  // complementary filter
-  // tau = DT*(A)/(1-A)
-  // = 0.48sec
-  gx = gx * 0.96 + ax * 0.04;
-  gy = gy * 0.96 + ay * 0.04;
+    glVertex3f(0.0, 0.0, 0.0)
+    glVertex3f(0.0, 2.0, 0.0)
 
-  // check if there is some kind of request 
-  // from the other side...
-  if(Serial.available())
-  {
-    char rx_char;
-    // dummy read
-    rx_char = Serial.read();
-    // we have to send data, as requested
-    if (rx_char == '.'){
-      digitalWrite(13, HIGH); //we are not using LED
-      Serial.print(gx, 2); //print gx upto 2 decimal places
-      Serial.print(", ");
-      Serial.print(gy, 2);
-      Serial.print(", ");
-      Serial.println(gz, 2);
-      digitalWrite(13, LOW);
-    }
-    // reset z gyro axis
-    if (rx_char == 'z'){
-      gz = 0;
-    }  
-  }
+    # arrow
+    glVertex3f(0.0, 2.0, 0.0)
+    glVertex3f(0.5, 1.5, 0.0)
 
-  //roll_servo.write(-gx+90);
+    glVertex3f(0.0, 2.0, 0.0)
+    glVertex3f(-0.5, 1.5, 0.0)
 
-  end_time = millis();
+    # Z
+    glColor3f(0.0, 0.0, 1.0) # Blue
 
-  // remaining time to complete sample time
-  delay(((1/FREQ) * 1000) - (end_time - start_time));
-  //Serial.println(end_time - start_time);
-}
+    glVertex3f(0.0, 0.0, 0.0)
+    glVertex3f(0.0, 0.0, 2.0)
 
+    # arrow
+    glVertex3f(0.0, 0.0, 2.0)
+    glVertex3f(0.0, 0.5, 1.5)
 
-void calibrate(){
+    glVertex3f(0.0, 0.0, 2.0)
+    glVertex3f(0.0, -0.5, 1.5)
+    glEnd()
 
-  int x;
-  long xSum = 0, ySum = 0, zSum = 0;
-  uint8_t i2cData[6]; 
-  int num = 500;
-  uint8_t error;
+         
+def read_data():
+    global ax, ay, az
+    ax = ay = az = 0.0
+    line_done = 0
 
-  for (x = 0; x < num; x++){
+    # request data by sending a dot
+    ser.write(b".") #* encode string to bytes
+    #while not line_done:
+    line = ser.readline() 
+    angles = line.split(b", ")
+    if len(angles) == 3:    
+        ax = float(angles[0])
+        ay = float(angles[1])
+        az = float(angles[2])
+        line_done = 1 
 
-    error = i2c_read(MPU6050_I2C_ADDRESS, 0x43, i2cData, 6); //requesting data from 6 registers of gyroscope 0x43 - 0x48
-    if(error!=0)
-    return;
+def main():
+    global yaw_mode
 
-    xSum += ((i2cData[0] << 8) | i2cData[1]); //Store first two bytes into xSum
-    ySum += ((i2cData[2] << 8) | i2cData[3]); //Store next two bytes into ySum
-    zSum += ((i2cData[4] << 8) | i2cData[5]); //Store next two bytes into zSum
-  }
+    video_flags = OPENGL|DOUBLEBUF
+    
+    pygame.init()
+    screen = pygame.display.set_mode((640,480), video_flags)
+    pygame.display.set_caption("Press Esc to quit, z toggles yaw mode")
+    resize(640,480)
+    init()
+    frames = 0
+    ticks = pygame.time.get_ticks()
+    while 1:
+        event = pygame.event.poll()
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()  #* quit pygame properly
+            break       
+        if event.type == KEYDOWN and event.key == K_z:
+            yaw_mode = not yaw_mode
+            ser.write(b"z")
+        read_data()
+        draw()
+        axes()
+      
+        pygame.display.flip()
+        frames = frames+1
 
-  //calculationg average offsets
-  gyrXoffs = xSum / num;
-  gyrYoffs = ySum / num;
-  gyrZoffs = zSum / num;
+    print(("fps:  %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks))))
+    ser.close()
 
-//  Serial.println("Calibration result:");
-//  Serial.print(gyrXoffs);
-//  Serial.print(", ");
-//  Serial.print(gyrYoffs);
-//  Serial.print(", ");
-//  Serial.println(gyrZoffs);
-  
-} 
+if __name__ == '__main__': main()
 
-void read_sensor_data(){
- uint8_t i2cData[14];
- uint8_t error;
- // read imu data
- error = i2c_read(MPU6050_I2C_ADDRESS, 0x3b, i2cData, 14); //mentioned in section 4.17 , request data from registers 0x3b - 0x48
- if(error!=0)
- return;
-
- // assemble 16 bit sensor data
- accX = ((i2cData[0] << 8) | i2cData[1]); //Store first two bytes into accX
- accY = ((i2cData[2] << 8) | i2cData[3]); //Store next two bytes into accY
- accZ = ((i2cData[4] << 8) | i2cData[5]); //Store next two bytes into accZ
-
- //skipped next two temp readings 0x41 and 0x 42
-
- gyrX = (((i2cData[8] << 8) | i2cData[9]) - gyrXoffs) / gSensitivity; //Store first two bytes into gyrX
- gyrY = (((i2cData[10] << 8) | i2cData[11]) - gyrYoffs) / gSensitivity; //Store next two bytes into gyrY
- gyrZ = (((i2cData[12] << 8) | i2cData[13]) - gyrZoffs) / gSensitivity; //Store last two bytes into gyrZ
- 
-}
-
-// ---- I2C routines
-
-int i2c_read(int addr, int start, uint8_t *buffer, int size)
-{
-  int i, n, error;
-
-  Wire.beginTransmission(addr);
-  n = Wire.write(start);
-  if (n != 1)
-  return (-10);
-
-  n = Wire.endTransmission(false);    // hold the I2C-bus
-  if (n != 0)
-  return (n);
-
-  // Third parameter is true: relase I2C-bus after data is read.
-  Wire.requestFrom(addr, size, true);
-  i = 0;
-  while(Wire.available() && i<size)
-  {
-    buffer[i++]=Wire.read();
-  }
-  if ( i != size)
-  return (-11);
-
-  return (0);  // return : no error
-}
-
-
-int i2c_write(int addr, int start, const uint8_t *pData, int size)
-{
-  int n, error;
-
-  Wire.beginTransmission(addr);
-  n = Wire.write(start);        // write the start address
-  if (n != 1)
-  return (-20);
-
-  n = Wire.write(pData, size);  // write data bytes
-  if (n != size)
-  return (-21);
-
-  error = Wire.endTransmission(true); // release the I2C-bus
-  if (error != 0)
-  return (error);
-
-  return (0);         // return : no error
-}
-
-
-int i2c_write_reg(int addr, int reg, uint8_t data)
-{
-  int error;
-  
-  error = i2c_write(addr, reg, &data, 1);
-  return (error);
-}
